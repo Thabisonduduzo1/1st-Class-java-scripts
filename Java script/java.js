cf7c281675ecef92c5759c51975771d405587536
Thabiso <thabisonduduzo1@gmail.com>
2025-09-22 17:06:27 +0200

    first commit

---
+++ Java script/java.js
@@ -0,0 +1,35 @@
+let myClass = 5
+console.log( myClass)
+
+const isAccepted = 15
+const isRejected = 15
+
+
+if( isAccepted =>15 ){console.log("The test is passed",isAccepted)} //if else
+
+else{console.log("the test failed score is -",isAccepted)};
+
+
+const age = 17;
+const driverLicence = (age >=18)? "Eligible" : "Not Eligable" ;      //tanery operator
+console.log(driverLicence);
+
+
+//logical operator
+
+let matric = "true";
+let application ="False";
+
+console.log( matric && application );  //and
+
+console.log(matric|| application);      //or
+
+console.log(!matric)            //not
+
+
+//arrithmetic
+
+let a =4
+let b = 5
+
+console.log( a+b)
