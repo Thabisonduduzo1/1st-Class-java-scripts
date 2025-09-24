31e80bff9a442625fc696d61f06eadfb2046863b
Thabiso <thabisonduduzo1@gmail.com>
2025-09-24 18:23:41 +0200

    commited

--- Java script/java.js
+++ Java script/java.js
@@ -67,11 +67,86 @@ console.log(newYear);
 
 
 //OTP Example
-const otpGeneration = Math.ceil( Math.random() * 10000)
+/*const otpGeneration = Math.ceil( Math.random() * 10000)
 const requestOTP = parseInt(prompt(`please enter the OTP provided -  ${otpGeneration}`))
 console.log(otpGeneration)
 const otpCheck = requestOTP === otpGeneration? alert("Successfully Authenticated") : alert("Unsuccessfull")
-console.log(otpCheck)
+console.log(otpCheck)*/
+
+
+//Questions On Conditional statenment (If Else and Else If)s
+
+ 
+
+/*
+
+
+
+5. Write a program that calculates a discount based on the purchase amount.Prices equal or over 100 discount have a discount of 20. Prices equal or over 50 have a discount of 10. Otherwise discount is 0*/
+
+
+
+//1. Write a program that determines whether a given number is positive or negative.
+
+/*const number = parseInt(prompt("Enter any number"))
+const numberCheck = number >= 0? alert("Positive Number") : alert("Negavite Number")
+console.log(numberCheck)*/
+
+
+//2. Write a program that checks if a number is even or odd.
+/*const number1 = parseInt(prompt("enter an number"))
+console.log(number1)
+ if(number1 % 2 == 0) { alert("Number is Even")}
+ else{ alert("Number is odd")}*/
+
+ //4. Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of 5, age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
+
+ const ageTicket = parseInt(prompt("Please enter your Age"))
+ if( ageTicket <12) { alert("Ticket price is R5")}
+ else if(ageTicket<18 && ageTicket>=12) {alert("Ticket price is R10")}
+ else if(ageTicket<60 && ageTicket>=18) {alert("Ticket price is R20")}
+  else if(ageTicket>=60) {alert("Ticket price is R15")}
+  else{"Entered age is not valid"}
+
+
+  //5. Write a program that calculates a discount based on the purchase amount.Prices equal or over 100 discount have a discount of 20. Prices equal or over 50 have a discount of 10. Otherwise discount is 0*/
+
+
+
+
+//function
+
+const myName = () => {console.log("Thabiso")}
+myName()
+
+//function with parameter
+
+const reusable = (name) => { console.log(name)}
+
+reusable("Thabsio")
+reusable("Ngcobo")
+
+
+
+//function of addding numbers
+
+const numberAdd = (n1,n2,n3) => {console.log(n1+n2+n3)}
+
+numberAdd(2,3,4)
+numberAdd(10,20,30)
+
+
+//funtion to tell highest of two numbers 
+//3. Write a program to determine the greater of two numbers.
+
+const highNum = (n1,n2) => {console.log( n1>n2? "N1 is greater":"N2 is greater" )}
+highNum(15,4)
+
+
+
+
+
+
    
 
 
